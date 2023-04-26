d8a02ca7954e11e220bea45a68eeb90e4b881e22
minhnhat02122001 <63940955+minhnhat02122001@users.noreply.github.com>
2023-04-26 14:33:08 +0700

    Doc 121 luan chuyen van ban toi chanh van phong screen (#36)
    * [DOC-121] Luan-chuyen-van-ban-toi-chanh-van-phong-screen
    
    * [DOC-121] change department name to vietnamese

--- db/src/main/023_init_department_table.sql
+++ db/src/main/023_init_department_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE "department"
 ) INHERITS ("doc_base_table");
 
 INSERT INTO "department" ("department_name", created_by, updated_by)
-VALUES ('BAN_CHI_HUY', 1, 1),
-       ('PHONG_KE_HOACH_DAU_TU', 1, 1);
+VALUES ('Ban Chỉ Huy', 1, 1),
+       ('Phòng kế hoạch đầu tư', 1, 1);
 
 ALTER TABLE "user" ADD COLUMN "department_id" BIGINT;
 ALTER TABLE "user" ADD CONSTRAINT "user_department_fk" FOREIGN KEY ("department_id") REFERENCES "department" ("id");
