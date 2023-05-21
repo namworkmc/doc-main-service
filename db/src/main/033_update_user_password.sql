b142c6a3235058ea80d2dfd063c2e1fbfa966199
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-05-21 18:14:36 +0700

    Hotfix update SQL

--- db/src/main/033_update_user_password.sql
+++ db/src/main/033_update_user_password.sql
@@ -1,5 +1,6 @@
 SET SEARCH_PATH TO doc_main;
 
+-- Update user5 to user10 password to 'user1'
 UPDATE "user"
 SET password = '$2a$10$fXwepveGHZt1poxgYBO7hecLyScDmnlV5c933nTGgN7bsUSk5RPb6'
 WHERE username IN ('user5', 'user6', 'user7', 'user8', 'user9', 'user10');
