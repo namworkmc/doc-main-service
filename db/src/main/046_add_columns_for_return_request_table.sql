cdce4a7d8408316fc7cf8fe9e121832944363fe4
Nguyen Duc Nam <namworkmc@gmail.com>
2023-07-30 22:41:43 +0700

    modify return request table, fix FK transfer history -> processing method instead of processing method -> transfer history

---
+++ db/src/main/046_add_columns_for_return_request_table.sql
@@ -0,0 +1,34 @@
+--liquibase formatted sql
+--changeset doc:046
+
+set search_path to doc_main;
+
+alter table return_request
+add column if not exists current_processing_user_id bigint not null,
+add column if not exists previous_processing_user_id bigint not null,
+add column if not exists incoming_doc_id bigint,
+add column if not exists outgoing_doc_id bigint;
+
+alter table return_request
+add constraint return_request_user_fk FOREIGN KEY (current_processing_user_id) REFERENCES doc_main."user"(id);
+
+alter table return_request
+add constraint return_request_user_fk_2 FOREIGN KEY (previous_processing_user_id) REFERENCES doc_main."user"(id);
+
+alter table return_request
+add constraint return_request_incoming_document_fk FOREIGN KEY (incoming_doc_id) REFERENCES doc_main."incoming_document"(id);
+
+alter table return_request
+add constraint return_request_outgoing_document_fk FOREIGN KEY (outgoing_doc_id) REFERENCES doc_main."outgoing_document"(id);
+
+
+alter table transfer_history add column return_request_id bigint;
+
+alter table transfer_history
+add constraint transfer_history_return_request_fk FOREIGN KEY (return_request_id) REFERENCES doc_main."return_request"(id);
+
+ALTER TABLE transfer_history DROP CONSTRAINT transfer_history_processing_method_fk;
+
+ALTER TABLE transfer_history
+    ADD CONSTRAINT
+        transfer_history_processing_method_fk FOREIGN KEY (processing_method_id) REFERENCES doc_main.processing_method(id);
