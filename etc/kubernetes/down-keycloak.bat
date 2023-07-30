bfc4e4f546aee1e1c50d89e6d21804b1d95eccbc
Nguyen Duc Nam <namworkmc@gmail.com>
2023-07-30 15:07:35 +0700

    Update image release v1.1.0

--- etc/kubernetes/down-keycloak.bat
+++
@@ -1,3 +0,0 @@
-kubectl delete -f ./doc-keycloak/doc-keycloak.config.yaml
-kubectl delete -f ./doc-keycloak/doc-keycloak.secret.yaml
-kubectl delete -f ./doc-keycloak/doc-keycloak.deployment.yaml
--- etc/kubernetes/up-keycloak.bat
+++
@@ -1,3 +0,0 @@
-kubectl apply -f ./doc-keycloak/doc-keycloak.config.yaml
-kubectl apply -f ./doc-keycloak/doc-keycloak.secret.yaml
-kubectl apply -f ./doc-keycloak/doc-keycloak.deployment.yaml
