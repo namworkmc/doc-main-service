7a394604f7f952df6d2242fe753a4ae4db84e355
Nguyen Duc Nam <66512409+namworkmc@users.noreply.github.com>
2023-08-08 07:16:52 +0700

    [DOC-271] remove required files (#128)

--- service/src/main/java/edu/hcmus/doc/mainservice/model/dto/OutgoingDocument/OutgoingDocumentWithAttachmentPostDto.java
+++ service/src/main/java/edu/hcmus/doc/mainservice/model/dto/OutgoingDocument/OutgoingDocumentWithAttachmentPostDto.java
@@ -2,8 +2,6 @@ package edu.hcmus.doc.mainservice.model.dto.OutgoingDocument;
 
 import java.util.List;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,7 +9,5 @@ import org.springframework.web.multipart.MultipartFile;
 public class OutgoingDocumentWithAttachmentPostDto {
   @NotBlank(message = "Invalid payload")
   private String outgoingDocumentPostDto;
-  @NotNull(message = "Attachments are required")
-  @Size(min = 1, max = 3, message = "The number of attachments must be between 1 and 3")
   private List<MultipartFile> attachments;
 }
